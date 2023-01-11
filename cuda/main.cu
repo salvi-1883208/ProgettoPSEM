2c89383a0659e4e3527aae06a2890de18e046de5
salvi-1883208 <salvi.1883208@studenti.uniroma1.it>
2023-01-11 19:24:03 +0100

    Fixed particles sometimes attaching to borders

--- cuda/main.cu
+++ cuda/main.cu
@@ -6,29 +6,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define gridSize 301
-#define MAX_ITERATIONS 9999999
+#define gridSize 701
 #define IMG_NAME "out.ppm"
 #define MAX_ITER 100000
-#define BLOCKS 30              // number of blocks
-#define THREADS_PER_BLOCK 128  // number of threads per block
+#define BLOCKS 16               // number of blocks
+#define THREADS_PER_BLOCK 1024  // number of threads per block
 
 // move the particle in the random direction
 __device__ void move_particle(int* x, int* y, int m);
 
+// calculate the number of overlapping particles
+int calc_over(int* grid, int size);
+
 // save the grid as a .ppm image
-void saveImage(int* grid, int size);
+int saveImage(int* grid, int size);
+
+// calculate the thread id
+__device__ int calc_id() {
+    return (blockIdx.z * gridDim.x * gridDim.y +
+            blockIdx.y * gridDim.x +
+            blockIdx.x) *
+               blockDim.x * blockDim.y * blockDim.z +
+           threadIdx.z * blockDim.x * blockDim.y +
+           threadIdx.y * blockDim.x +
+           threadIdx.x;
+}
 
 // kernel to set up the seed for each thread
 __global__ void setup_kernel(curandState* state) {
-    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    int id = calc_id();
     curand_init(1234, id, 0, &state[id]);
 }
 
 // kernel to perform the dla algorithm
 __global__ void dla_kernel(int* grid, int* skipped, curandState* state) {
     // calculate thread id
-    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    int id = calc_id();
 
     // initialize the starting position of the particle
     int x;
@@ -38,28 +51,28 @@ __global__ void dla_kernel(int* grid, int* skipped, curandState* state) {
     do {
         x = curand(&state[id]) % gridSize;
         y = curand(&state[id]) % gridSize;
-    } while (grid[y * gridSize + x] == 1);
+    } while (grid[y * gridSize + x] > 0);
 
     // print the starting position of the particle
-    printf("Thread (%d, %d) started (%d, %d)\n", blockIdx.x, id, x, y);
+    // printf("Thread (%d, %d) started (%d, %d)\n", blockIdx.x, id, x, y);
 
     // iterate until the particle is attached to the grid or it did more than MAX_ITER number of iterations
     for (int i = 0; i < MAX_ITER; i++) {
         // if the particle not outside the grid &&
         // if the particle is close to an already stuck particle
-        if (!(y < 0 || y > (gridSize - 1) || x < 0 || x > (gridSize - 1)) &&  // in bounds
-            (grid[(y - 1) * gridSize + (x - 1)] == 1 ||                       // top left
-             grid[(y - 1) * gridSize + x] == 1 ||                             // top
-             grid[(y - 1) * gridSize + (x + 1)] == 1 ||                       // top right
-             grid[y * gridSize + (x - 1)] == 1 ||                             // left
-             grid[y * gridSize + (x + 1)] == 1 ||                             // right
-             grid[(y + 1) * gridSize + (x - 1)] == 1 ||                       // bottom left
-             grid[(y + 1) * gridSize + x] == 1 ||                             // bottom
-             grid[(y + 1) * gridSize + (x + 1)] == 1)) {                      // bottom right
+        if (!(y <= 0 || y >= (gridSize - 1) || x <= 0 || x >= (gridSize - 1)) &&  // in bounds
+            (grid[(y - 1) * gridSize + (x - 1)] > 0 ||                            // top left
+             grid[(y - 1) * gridSize + x] > 0 ||                                  // top
+             grid[(y - 1) * gridSize + (x + 1)] > 0 ||                            // top right
+             grid[y * gridSize + (x - 1)] > 0 ||                                  // left
+             grid[y * gridSize + (x + 1)] > 0 ||                                  // right
+             grid[(y + 1) * gridSize + (x - 1)] > 0 ||                            // bottom left
+             grid[(y + 1) * gridSize + x] > 0 ||                                  // bottom
+             grid[(y + 1) * gridSize + (x + 1)] > 0)) {                           // bottom right
 
             // if the particle is close to an already stuck particle, attach it to the grid
-            grid[y * gridSize + x] = 1;
-            printf("Thread (%d, %d) finished (%d, %d)\n", blockIdx.x, id, x, y);
+            atomicAdd(&grid[y * gridSize + x], 1);
+            // printf("Thread (%d, %d) finished (%d, %d)\n", blockIdx.x, id, x, y);
             return;
         }
 
@@ -94,7 +107,7 @@ __global__ void dla_kernel(int* grid, int* skipped, curandState* state) {
     // if the particle did more than MAX_ITER number of iterations, skip it
     atomicAdd(skipped, 1);
 
-    printf("Thread (%d, %d) skipped\n", blockIdx.x, id);
+    // printf("Thread (%d, %d) skipped\n", blockIdx.x, id);
 
     return;
 }
@@ -103,9 +116,11 @@ int main(void) {
     int* grid = (int*)malloc(gridSize * gridSize * sizeof(int));
     int* skipped;
     curandState* d_state;
+    int blocks = BLOCKS;
+    int threads_per_block = THREADS_PER_BLOCK;
 
     // allocate the array of the random states in the device memory
-    cudaMalloc((void**)&d_state, BLOCKS * THREADS_PER_BLOCK * sizeof(curandState));
+    cudaMalloc((void**)&d_state, blocks * threads_per_block * sizeof(curandState));
 
     // allocate the grid for both the host and the device
     cudaMallocManaged((void**)&grid, gridSize * gridSize * sizeof(int), cudaMemAttachGlobal);
@@ -125,7 +140,7 @@ int main(void) {
     grid[gridSize * (gridSize / 2) + (gridSize / 2)] = 1;
 
     // launch the kernel to set up the seed for each thread
-    setup_kernel<<<BLOCKS, THREADS_PER_BLOCK>>>(d_state);
+    setup_kernel<<<blocks, threads_per_block>>>(d_state);
 
     // wait for the kernel to finish
     cudaDeviceSynchronize();
@@ -133,11 +148,32 @@ int main(void) {
     // time execution start
     clock_t start = clock();
 
-    // launch the kernel to perform the dla algorithm
-    dla_kernel<<<BLOCKS, THREADS_PER_BLOCK>>>(grid, skipped, d_state);
+    int over = 0;
 
-    // wait for the kernel to finish
-    cudaDeviceSynchronize();
+    do {  // launch the kernel to perform the dla algorithm
+        dla_kernel<<<blocks, threads_per_block>>>(grid, skipped, d_state);
+
+        // wait for the kernel to finish
+        cudaDeviceSynchronize();
+
+        // get the number of overlapping particles
+        over = calc_over(grid, gridSize);
+
+        printf("Overlapping: %d\n", over);
+
+        // if there are more than 1024 overlapping particles
+        if (over > 1024) {
+            // calculate the number of blocks
+            blocks = floor(over / 1024) + 1;
+            // calculate the number of threads per block
+            threads_per_block = ceil(((float)over) / ((float)blocks));
+        } else {
+            // else run only one block with just overlapping threads
+            blocks = 1;
+            threads_per_block = over;
+        }
+        // if there are overlapping particles launch the kernel again until there are none
+    } while (over > 0);
 
     // time execution end
     clock_t end = clock();
@@ -148,7 +184,7 @@ int main(void) {
     // print the number of skipped particles
     printf("Skipped: %d\n", *skipped);
 
-    // save the grid as a .ppm image
+    // save the grid as a .ppm image and get the number of skipped particles
     saveImage(grid, gridSize);
 
     // free the memory
@@ -157,6 +193,17 @@ int main(void) {
     cudaFree(d_state);
 
     return 0;
+}
+
+int calc_over(int* grid, int size) {
+    int tot = 0;
+    for (int i = 0; i < size; ++i)
+        for (int j = 0; j < size; ++j)
+            if (grid[i * size + j] > 1) {
+                tot++;
+                grid[i * size + j] = 1;
+            }
+    return tot;
 }
 
 // move the particle in the random direction
@@ -193,7 +240,7 @@ __device__ void move_particle(int* x, int* y, int m) {
     }
 }
 
-void saveImage(int* grid, int size) {
+int saveImage(int* grid, int size) {
     int count = 0;
     // save image to .ppm file
     int i, j;
@@ -209,20 +256,21 @@ void saveImage(int* grid, int size) {
                     color[2] = 255; /* blue */
                     count++;
                     break;
-                case 2:             // seed
-                    color[0] = 255; /* red */
-                    color[1] = 0;   /* green */
-                    color[2] = 0;   /* blue */
-                    break;
-                // case 3:             // circle close to stuck structure
-                //     color[0] = 0;   /* red */
+                // case 2:             // seed
+                //     color[0] = 255; /* red */
                 //     color[1] = 0;   /* green */
-                //     color[2] = 255; /* blue */
+                //     color[2] = 0;   /* blue */
                 //     break;
-                default:          // empty spots
+                case 0:           // empty spots
                     color[0] = 0; /* red */
                     color[1] = 0; /* green */
                     color[2] = 0; /* blue */
+                    break;
+                default:            // overlapping
+                    color[0] = 255; /* red */
+                    color[1] = 0;   /* green */
+                    color[2] = 0;   /* blue */
+                    count++;
                     break;
             }
             (void)fwrite(color, 1, 3, fp);
@@ -230,5 +278,7 @@ void saveImage(int* grid, int size) {
     }
     (void)fclose(fp);
 
-    printf("Saved image containing %d particles\n", count);
+    printf("Saved image containing %d particles\n", count - 1);
+
+    return count;
 }
